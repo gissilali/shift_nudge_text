49ca0c5f61b5842a11e7e970b7af8862cda75aad
Gibson Silali <gibsonsilali@gmail.com>
2020-09-10 16:00:42 +0300

    styling interactive text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <p>Crossroads and
-      <span class="text-interactable">niggas</span> be wylin'
+    <p>
+      Crossroads and
+      <span class="text-interactive">
+        <span class="underline"></span>
+        <span>niggas</span>
+      </span> be wylin'
     </p>
   </div>
 </template>
@@ -25,5 +29,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.text-interactive {
+  position: relative;
+  display: inline-block;
+  z-index: 200;
+  cursor: pointer;
+}
+.text-interactive .underline {
+  display: inline-block;
+  position: absolute;
+  background: rgba(45, 56, 201, 0.3);
+  width: 100%;
+  height: 4px;
+  bottom: -8px;
+  left: 0;
+  z-index: 20;
+}
+.text-interactive span:not(.underline) {
+  z-index: 20000;
+}
+.text-interactive:hover .underline {
+  height: 30px;
+}
+.underline {
+  transition: all 300ms;
 }
 </style>
